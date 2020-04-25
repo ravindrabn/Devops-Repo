02365eae1428d9cbc1ddd3402c5cabe01c4fc416
ravindrabn <63568266+ravindrabn@users.noreply.github.com>
2020-04-25 22:09:13 +1000

    Added Test project

---
+++ MyDevopsDemo/MathsOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDevopsDemo
+{
+    public class MathsOperation
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply (int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide (int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ MyDevopsDemoTEST/MathsOperationTest.cs
@@ -0,0 +1,38 @@
+﻿using MyDevopsDemo;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDevopsDemoTEST
+{
+    class MathsOperationTest
+    {
+        [Test()]
+        public void AddTest()
+        {
+            MathsOperation mo = new MathsOperation();
+            mo.Add(10, 20);
+            Assert.AreEqual(true,30);
+        }
+        public void SubtractTest()
+        {
+            MathsOperation mo = new MathsOperation();
+            mo.Subtract(20, 10);
+            Assert.AreEqual(true, 10);
+        }
+        public void Multiply()
+        {
+            MathsOperation mo = new MathsOperation();
+            mo.Add(10, 20);
+            Assert.AreEqual(true, 200);
+        }
+        public void DivideTest()
+        {
+            MathsOperation mo = new MathsOperation();
+            mo.Add(20, 10);
+            Assert.AreEqual(true, 2);
+        }
+
+    }
+}
